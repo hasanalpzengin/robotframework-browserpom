e3eb4f04db821d580edec4dbeff1ef28c7e1537d
Hasan Alp Zengin <hasalp38@gmail.com>
2025-03-03 20:53:25 +0100

    fix: linters

--- BrowserPOM/pageobject.py
+++ BrowserPOM/pageobject.py
@@ -1,9 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
 import robot.api
-from robot.libraries.BuiltIn import BuiltIn, RobotNotRunningError
-
 from Browser import Browser
+from robot.libraries.BuiltIn import BuiltIn, RobotNotRunningError
 
 
 class PageObject:
