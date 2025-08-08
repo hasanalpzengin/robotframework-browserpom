731095fbe916aab9cbe1285911ddcb3610f50290
hasanalpzengin <hasalp38@gmail.com>
2025-08-08 21:36:04 +0200

    fix: ruff check

--- BrowserPOM/decorator.py
+++ BrowserPOM/decorator.py
@@ -1,8 +1,9 @@
+# ruff: noqa
 def on_error_trigger(method):
     def wrapper(self, *args, **kwargs):
         try:
             return method(self, *args, **kwargs)
-        except Exception as e:
+        except Exception:
             self.browser.keyword_error("body")
             raise
     return wrapper
